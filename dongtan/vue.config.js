4a9ffc091f3d78d8496ebf167bdec63975754d24
infiniblue-JTB <infiniblue@naver.com>
2026-05-06 21:43:01 +0900

    fix: refactor pre-rendering config and update _redirects for Cloudflare

--- dongtan/vue.config.js
+++ dongtan/vue.config.js
@@ -1,21 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 const PrerendererWebpackPlugin = require('@prerenderer/webpack-plugin')
+const PuppeteerRenderer = require('@prerenderer/renderer-puppeteer')
 
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/',
-  configureWebpack: {
-    plugins: [
-      new PrerendererWebpackPlugin({
-        routes: ['/', '/kids', '/food', '/cafe', '/health', '/blog', '/fun', '/board', '/about', '/privacy'],
-        renderer: '@prerenderer/renderer-puppeteer',
-        rendererOptions: {
-          renderAfterDocumentEvent: 'render-event',
-          headless: true,
-          args: ['--no-sandbox', '--disable-setuid-sandbox']
-        }
-      })
-    ]
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new PrerendererWebpackPlugin({
+          staticDir: path.join(__dirname, 'dist'),
+          routes: ['/', '/kids', '/food', '/cafe', '/health', '/blog', '/fun', '/board', '/about', '/privacy'],
+          renderer: new PuppeteerRenderer({
+            renderAfterDocumentEvent: 'render-event',
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+          })
+        })
+      )
+    }
   }
 })
